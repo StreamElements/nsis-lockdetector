3b859ce063278dd66c99f3678c61b0441c3a12c4
Ilya Melamed <ilya77@gmail.com>
2018-08-12 19:41:19 +0300

    Visual facelift

--- resource.h
+++ resource.h
@@ -3,17 +3,20 @@
 // Used by pslist.rc
 //
 #define IDD_ListDialog                  9
+#define IDB_BITMAP1                     105
+#define IDI_ICON                        106
 #define IDC_LIST_TITLE                  1001
 #define IDC_LIST_TITLE2                 1002
+#define IDC_STATIC2                     1002
 #define IDC_LIST                        1212
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
